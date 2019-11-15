fdf920adfa6a3c19f195878664afa90e05ee5b65
alex_di <aalleexx8686@gmail.com>
2019-11-15 04:29:58 +0300

    fixed codestyle

--- modules/task_2/Korobeinikov_a_Jacobi_method/Jacoby_method.h
+++ modules/task_2/Korobeinikov_a_Jacobi_method/Jacoby_method.h
@@ -1,6 +1,6 @@
 // Copyright 2019 Korobeinikov Aleksei
-#ifndef MODULES_TASK_2_KOROBEINIKOV_A_JACOBI_METHOD_JACOBI_METHOD_H_
-#define MODULES_TASK_2_KOROBEINIKOV_A_JACOBI_METHOD_JACOBI_METHOD_H_
+#ifndef MODULES_TASK_2_KOROBEINIKOV_A_JACOBI_METHOD_JACOBY_METHOD_H_
+#define MODULES_TASK_2_KOROBEINIKOV_A_JACOBI_METHOD_JACOBY_METHOD_H_
 #include <vector>
 
 double scalar(const std::vector<double>& a, const std::vector<double>& b);
@@ -15,4 +15,4 @@ std::vector<double> Jacobi_Solve(const std::vector <double>& A,
 std::vector <double> GetSolveOfTask(std::vector<double> matrix,
     std::vector <double> b, int Size, double eps);
 
-#endif  // MODULES_TASK_2_KOROBEINIKOV_A_JACOBI_METHOD_JACOBI_METHOD_H_
+#endif  // MODULES_TASK_2_KOROBEINIKOV_A_JACOBI_METHOD_JACOBY_METHOD_H_
